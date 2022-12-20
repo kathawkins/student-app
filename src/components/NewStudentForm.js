c21fc2bceb0912ff76046afb8b4c5dbd99a48927
kathawkins <kmoreno1205@gmail.com>
2022-12-19 17:39:04 -0700

    added new student form

---
+++ src/components/NewStudentForm.js
@@ -0,0 +1,17 @@
+const NewStudentForm = () => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="fullName">Name:</label>
+        <input name="fullName" />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input name="email" />
+      </div>
+      <input type="submit" value="Add Student" />
+    </form>
+  );
+};
+
+export default NewStudentForm;
